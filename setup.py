1b101d99fe7458f010c892331745f22ec2fce8f5
QvonR <quirin.von.rekowski@web.de>
2022-11-28 14:20:20 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
    name='snowflake',
    version='1.0',
-   description='Draws snowflakes in random colours and on random positions',
-   author='Quirin von Rekowski',
-   author_email='quirin.von.rekowski@fau.de',
-   packages=['snowflake'],  # would be the same as name
-   install_requires=['turtle', 'numpy'], #external packages acting as dependencies
+   py_modules=['snowflake']
 )
